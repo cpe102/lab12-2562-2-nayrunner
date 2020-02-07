8ff4dd00067eb92579a058acd1851094fd9f02d3
nayrunner <59276659+nayrunner@users.noreply.github.com>
2020-02-07 16:55:35 +0700

    Update lab12_1.cpp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include<math.h>
 using namespace std;
 
 void stat(const double[],int,double []);
@@ -18,3 +19,24 @@ int main()
 }
 
 //Write definition of stat() here 
+void stat(const double A[],int N,double B[]){
+   double max= A[0] ,min=A[0],avr,xx,sd;
+  for(int i = 0;i<=N;i++){
+       avr += A[i];
+      
+       if(max<=A[i]){max = A[i];}
+       if(min>=A[i]){min = A[i];}
+      
+      
+       xx += (A[i]*A[i]);
+   }
+  
+   double mean = avr / N;
+   sd = (xx/N)-(mean*mean);
+   sd = sqrt(sd);
+
+   B[0] = mean;
+   B[1] = sd;
+   B[2] = max;
+   B[3] = min;
+}
